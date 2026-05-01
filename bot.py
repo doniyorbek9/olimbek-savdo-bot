2a99ec74c9eb5726cfc5a97e53098ef81c1b7b9a
doniyorbek9 <gdoniyorbek8@gmail.com>
2026-05-01 23:00:57 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,16 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot ishlayapti!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+Thread(target=run).start()
 import asyncio
 import logging
 import random
